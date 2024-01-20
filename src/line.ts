e8c97d3b374d0e4347dc012a6631bba05c1f8f8d
齋藤孝一 <saito_koichi@saitoukouichinoMacBook-Pro.local>
2024-01-20 15:56:36 +0900

    検索にflexmsgを返す

--- src/line.ts
+++ src/line.ts
@@ -28,64 +28,68 @@ export class Line {
   }
 
   public async replyFlexMessage(text: string, replyToken: string): Promise<Response | null> {
-    const bubbleList = {
-      "type": "bubble",
-      "styles": {
-        "header": {
-          "backgroundColor": "#ffaaaa"
-        },
-        "body": {
-          "backgroundColor": "#aaffaa"
-        },
-        "footer": {
-          "backgroundColor": "#aaaaff"
+    const flexMessage = [
+      {
+        "type": "flex",
+        "altText": text,
+        "contents": {
+          "type": "carousel",
+          "contents": [
+            {
+              "type": "bubble",
+              "styles": {
+                "header": {
+                  "backgroundColor": "#ffaaaa"
+                },
+                "body": {
+                  "backgroundColor": "#aaffaa"
+                },
+                "footer": {
+                  "backgroundColor": "#aaaaff"
+                }
+              },
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "header"
+                  }
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://example.com/flex/images/image.jpg",
+                "size": "full",
+                "aspectRatio": "2:1"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "body"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "footer"
+                  }
+                ]
+              }
+            }
+          ]
         }
-      },
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "header"
-          }
-        ]
-      },
-      "hero": {
-        "type": "image",
-        "url": "https://example.com/flex/images/image.jpg",
-        "size": "full",
-        "aspectRatio": "2:1"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "body"
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "footer"
-          }
-        ]
       }
-    };
-    const flexMessage = {
-      "type": "flex",
-      "altText": "口コミランキング順お店情報",
-      "contents": {
-        "type": "carousel",
-        "contents": bubbleList
-      }
-    };
+    ];
+    // このmessages変数は、LINE Messaging APIでメッセージを送信する際に使用できます。
     console.log(flexMessage);
     console.log(replyToken);
     return await fetch(`${this.baseUrl}/v2/bot/message/reply`, {
@@ -93,7 +97,7 @@ export class Line {
       headers: this.headers,
       body: JSON.stringify({
         replyToken: replyToken,
-        messages: [flexMessage],
+        messages: flexMessage,
       }),
     }).catch((err) => {
       console.log(`LINE API error: ${err}`);
